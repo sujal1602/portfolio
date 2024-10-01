eacabf46cba69d851615e051ae2b339d813b23d0
sujal <121374315+sujal1602@users.noreply.github.com>
2024-10-01 23:41:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+    var typed = new Typed('#element', {
+      strings: ['java , C ',' &amp;  Web developer.'],
+      typeSpeed: 100,
+      backSpeed:20,
+      loop:true
+    });
+    document.getElementById("btn").onclick = function () {
+      window.location.href = "http://www.linkedin.com/in/sujal-dwivedi-53bb2b235";
+    };
+    document.getElementById("btn1").onclick = function () {
+      window.location.href = "sujalMainresume.pdf";
+    };
+    document.getElementById("btn2").onclick = function () {
+      window.location.href = "https://github.com/sujal1602";
+    };
